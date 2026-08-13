0e486aa877fdd2823f97a5bd72b7a1127803faf2
sharithsaavedra1 <mimisaavedra09@gmail.com>
2026-08-13 12:56:25 -0500

    chat flotante

---
+++ appmovilrenta/modules/support/constants/tawk.config.ts
@@ -0,0 +1,7 @@
+export const TAWK_CONFIG = {
+  propertyId: "64bcb933cc26a871b02a991b",
+  widgetId: "1h5v9v4b3",
+  get chatUrl() {
+    return `https://tawk.to/chat/${this.propertyId}/${this.widgetId}`;
+  }
+};
